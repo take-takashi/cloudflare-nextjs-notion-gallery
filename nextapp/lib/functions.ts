2c98cb73b7b4416ddf6d022efcc9322666b36695
TAKE Takashi <nakamizo.takashi@gmail.com>
2022-11-12 16:12:32 +0000

    fix hello

--- nextapp/lib/functions.ts
+++ nextapp/lib/functions.ts
@@ -3,7 +3,7 @@
  * @param json responseするjson
  * @returns 200 OK Response
  */
-export default function responseOk(json: JSON): Response {
+export default function responseOk(json: Object): Response {
   const jsonString = JSON.stringify(json)
 
   return new Response(jsonString, {
--- nextapp/pages/api/hello.ts
+++ nextapp/pages/api/hello.ts
@@ -1,15 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextRequest } from 'next/server'
+import responseOk from '../../lib/functions'
 
 export const config = {
   runtime: 'experimental-edge',
 }
 
 export default function handler(req: NextRequest) {
-  return new Response(JSON.stringify({ name: 'John Doe' }), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
+  return responseOk({ name: 'John Doeee' })
 }
