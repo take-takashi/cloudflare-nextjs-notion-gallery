3dc7dc7d09f3c2bc9f952ae2f57c4f94c61e9a3a
TAKE Takashi <nakamizo.takashi@gmail.com>
2022-11-12 16:05:36 +0000

    feat(COM): add functions

---
+++ nextapp/lib/functions.ts
@@ -0,0 +1,15 @@
+/**
+ *
+ * @param json responseするjson
+ * @returns 200 OK Response
+ */
+export default function responseOk(json: JSON): Response {
+  const jsonString = JSON.stringify(json)
+
+  return new Response(jsonString, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
